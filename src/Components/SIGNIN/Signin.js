f68422bfe353d145caf9b2d6b9808ff974f0ba92
bathulapraka <bathulaprakash406@gmail.com>
2024-06-17 18:02:35 +0530

    restaurent

---
+++ src/Components/SIGNIN/Signin.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import "../SIGNIN/Signin.css"
+export default function Signin() {
+  return (
+    <div>
+      <div className="main">  	
+        <input type="checkbox" id="chk" aria-hidden="true" />
+
+        <div className="login">
+          <form className="form">
+            <label htmlFor="chk" aria-hidden="true">Log in</label>
+            <input className="input" type="email" name="email" placeholder="Email" required />
+            <input className="input" type="password" name="pswd" placeholder="Password" required />
+            <button type="submit">Log in</button>
+          </form>
+        </div>
+
+        <div className="register">
+          <form className="form">
+            <label htmlFor="chk" aria-hidden="true">Register</label>
+            <input className="input" type="text" name="txt" placeholder="Username" required />
+            <input className="input" type="email" name="email" placeholder="Email" required />
+            <input className="input" type="password" name="pswd" placeholder="Password" required />
+            <button type="submit">Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
